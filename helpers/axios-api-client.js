b3962629dd2127afbffb344d5c0a2160c9763210
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-03-09 15:27:06 +0300

    Add necessary api function

--- helpers/axios-api-client.js
+++ helpers/axios-api-client.js
@@ -4,7 +4,26 @@ export async function getSliders() {
   const { data } = await axiosClient.get('sliders');
   return data;
 }
-export async function getProductsId(id) {
+export async function getProduct(id) {
   const { data } = await axiosClient.get(`products/${id}`);
+  return data;
+}
+export async function getCategories() {
+  const { data } = await axiosClient.get(`categories`);
+  return data;
+}
+
+export async function getCategory(id) {
+  const { data } = await axiosClient.get(`categories/${id}`);
+  return data;
+}
+
+export async function getFavorites() {
+  const { data } = await axiosClient.get(`favorites`);
+  return data;
+}
+
+export async function addToFavorite() {
+  const { data } = await axiosClient.post(`favorites`);
   return data;
 }
